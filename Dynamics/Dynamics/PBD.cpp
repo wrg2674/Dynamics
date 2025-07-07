ba8e76472df70bd3d7524db715d26aeb2ea82031
wrg26474 <wrg267499@naver.com>
2025-07-07 20:15:49 +0900

    프로젝트 생성

---
+++ Dynamics/Dynamics/PBD.cpp
@@ -0,0 +1,17 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <learnopengl/shader.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+
+int main() {
+		
+}
